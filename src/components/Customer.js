033c196fe9da2cb94b257ba9daa1f436a4b0f13b
tonysu <su.yuy@husky.neu.edu>
2020-12-06 23:44:39 -0500

    complete CRUD of customer

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
-import { getAllCustomer, getCustomerById, addCustomer, deleteCustomerById, updateCustomer } from '../services/CustomerService';
+import { getAllCustomer, addCustomer} from '../services/CustomerService';
+import { Link } from 'react-router-dom';
 
 class Customer extends Component {
 
     state = {
-        customers: []
+        customers: [],
+        customer: {
+            id: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: ""
+        }
     }
 
     getAllCustomer = () => {
         getAllCustomer().then(response => {
             if(response.responseCode === 200) {
                 const customers = response.responseObj;
-                console.log(customers)
                 this.setState({
                     customers: customers
                 })
             }
+        })
+    }
+
+    addCustomer = () => {
+        if(this.state.customer.id === "") {
+            alert("Id can't be empty!")
+            return;
+        }
+        addCustomer(this.state.customer).then(response => {
+            if(response.responseCode === 200) {
+                this.getAllCustomer();
+            }
+        })
+    }
+
+    onChangeHandler = (event, field) => {
+        let customer = this.state.customer;
+        let value = event.target.value;
+        switch(field) {
+            case "id":
+                customer.id = value.trim();
+                break;
+            case "firstName":
+                customer.firstName = value.trim();
+                break;
+            case "lastName":
+                customer.lastName = value.trim();
+                break;
+            case "email":
+                customer.email = value.trim();
+                break;
+            case "phone":
+                customer.phone = value.trim();
+                break;
+            default:
+                console.log("No input field match.")
+        }
+        this.setState({
+            customer: customer,
         })
     }
 
@@ -28,12 +74,42 @@ class Customer extends Component {
             this.props.agent !== null ? 
                 <div>
                     <h1>Customer Page</h1>
+                    <h2>New Customer</h2>
+                    <div className="row">
+                        <div className="col-4">
+                            <label htmlFor="id">Id: </label>
+                            <input id="id" value={this.state.customer.id} onChange={(event) => this.onChangeHandler(event, "id")}/>
+                            <br />
+                            <label htmlFor="firstName">Firset Name: </label>
+                            <input id="firstName" value={this.state.customer.firstName} onChange={(event) => this.onChangeHandler(event, "firstName")}/>
+                            <br />
+                            <label htmlFor="lastName">Last Name: </label>
+                            <input id="lastName" value={this.state.customer.lastName} onChange={(event) => this.onChangeHandler(event, "lastName")}/>
+                            <br />
+                        </div>
+                        <div className="col-4">
+                            <label htmlFor="email">Email: </label>
+                            <input id="email" value={this.state.customer.email} onChange={(event) => this.onChangeHandler(event, "email")}/>
+                            <br />
+                            <label htmlFor="phone">Phone: </label>
+                            <input id="phone" value={this.state.customer.phone} onChange={(event) => this.onChangeHandler(event, "phone")}/>
+                        </div>
+                        
+                    </div>
+                    <button onClick={this.addCustomer}>Create</button>
+
                     {this.state.customers.map(customer => 
-                        <div key={customer.id}>
-                            <h5>Id: {customer.id}</h5>
-                            <h5>Name: {customer.firstName} {customer.lastName}</h5>
-                            <h5>Email: {customer.email}</h5>
-                            <h5>Phone: {customer.phone}</h5>
+                        <div className="row" key={customer.id}>
+                            <div className="col-4">
+                                <h5>Id: {customer.id}</h5>
+                                <h5>Name: {customer.firstName} {customer.lastName}</h5>
+                                <Link to={`/customers/${customer.id}`}>Edit</Link>
+                            </div>
+                            <div className="col-4">
+                                <h5>Email: {customer.email}</h5>
+                                <h5>Phone: {customer.phone}</h5>
+                                
+                            </div>
                         </div>
                     )}
                 </div>
--- src/components/CustomerDetail.js
+++ src/components/CustomerDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getCustomerById, deleteCustomerById, updateCustomer } from '../services/CustomerService';
+// import history from '../history';
 
 class CustomerDetail extends Component {
 
@@ -11,11 +12,61 @@ class CustomerDetail extends Component {
         getCustomerById(id).then(response => {
             if(response.responseCode === 200) {
                 const customer = response.responseObj;
-                console.log(customer)
                 this.setState({
                     customer: customer
                 })
             }
+        })
+    }
+
+    deleteCustomer = () => {
+        deleteCustomerById(this.state.customer.id).then(response => {
+            if(response.responseCode === 200) {
+                this.setState({
+                    customer: null
+                })
+            }
+            alert(response.message);
+        })
+    }
+
+    updateCustomer = () => {
+        const customer = this.state.customer;
+        updateCustomer(customer.id, customer).then(response => {
+            if(response.responseCode === 200) {
+                const customer = response.responseObj;
+                this.setState({
+                    customer: customer
+                })
+            }
+            alert(response.message);
+        })
+    }
+
+    onChangeHandler = (event, field) => {
+        let customer = this.state.customer;
+        let value = event.target.value;
+        switch(field) {
+            case "id":
+                customer.id = value;
+                break;
+            case "firstName":
+                customer.firstName = value;
+                break;
+            case "lastName":
+                customer.lastName = value;
+                break;
+            case "email":
+                customer.email = value;
+                break;
+            case "phone":
+                customer.phone = value;
+                break;
+            default:
+                console.log("No input field match.")
+        }
+        this.setState({
+            customer: customer,
         })
     }
 
@@ -32,11 +83,31 @@ class CustomerDetail extends Component {
                     <h1>CustomerDetail Page</h1>
                     {this.state.customer !== null && 
                         <div>
-                            <h5>Id: {this.state.customer.id}</h5>
-                            <h5>Firset Name: {this.state.customer.firstName}</h5>
-                            <h5>Last Name: {this.state.customer.lastName}</h5>
-                            <h5>Email: {this.state.customer.email}</h5>
-                            <h5>Phone: {this.state.customer.phone}</h5>
+                            <label htmlFor="id">Id: </label>
+                            <input id="id" value={this.state.customer.id} onChange={(event) => this.onChangeHandler(event, "id")} disabled/>
+                            <br />
+                            <label htmlFor="firstName">Firset Name: </label>
+                            <input id="firstName" value={this.state.customer.firstName} onChange={(event) => this.onChangeHandler(event, "firstName")}/>
+                            <br />
+                            <label htmlFor="lastName">Last Name: </label>
+                            <input id="lastName" value={this.state.customer.lastName} onChange={(event) => this.onChangeHandler(event, "lastName")}/>
+                            <br />
+                            <label htmlFor="email">Email: </label>
+                            <input id="email" value={this.state.customer.email} onChange={(event) => this.onChangeHandler(event, "email")}/>
+                            <br />
+                            <label htmlFor="phone">Phone: </label>
+                            <input id="phone" value={this.state.customer.phone} onChange={(event) => this.onChangeHandler(event, "phone")}/>
+                            <br />
+                            <br />
+
+                            <button onClick={this.deleteCustomer}>Delete</button>
+                            <button onClick={this.updateCustomer}>Update</button>
+
+                        </div>
+                    }
+                    {this.state.customer === null &&
+                        <div>
+                            <h1>This customer does not exist</h1>
                         </div>
                     }
                 </div>
